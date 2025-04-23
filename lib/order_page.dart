5fde35ed8b9023082da29e046ae0ffc27505aa8b
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-04-23 21:19:58 +0700

    membuat halaman bernama OrderPage sebagai widget bisa di rubah

---
+++ lib/order_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:assigment4/detail_order_page.dart';
+
+class OrderPage extends StatefulWidget {
+  const OrderPage({super.key});
+
+  @override
+  State<OrderPage> createState() => _OrderPageState();
+}
